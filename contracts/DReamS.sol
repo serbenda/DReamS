7587e442b13097e7ebc851db81b174289add3cc5
serbenda <57532210+serbenda@users.noreply.github.com>
2022-12-19 19:47:37 +0100

    Update DReamS.sol

--- contracts/DReamS.sol
+++ contracts/DReamS.sol
@@ -1,168 +1,143 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.9;
 
-// Import the required contracts from the OpenZeppelin library
+// Importamos librerías de contratos de OpenZeppelin para agregar funcionalidades adicionales al contrato
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// Define the DReamS contract as a subclass of ERC721, ERC721URIStorage, and Ownable
-contract DReamS is ERC721, ERC721URIStorage, Ownable {
-    // Use the Counter struct from the Counters library
+// Definimos el contrato MyToken que hereda de varios contratos de OpenZeppelin
+// y del estándar ERC-721 para tokens no fungibles
+contract MyToken is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
+    
+    // Utilizamos la librería Counters de OpenZeppelin para llevar un control del contador de tokens emitidos
     using Counters for Counters.Counter;
-
-    // Define the supply variable as a public variable of type uint256
+    
+    // Declaramos una variable pública para llevar un registro de la cantidad de tokens emitidos
     uint256 public supply;
-
-    // Define the _tokenIdCounter storage variable as a private Counter
+    
+    // Declaramos una variable privada de tipo Counters.Counter para llevar un control del contador de tokens emitidos
     Counters.Counter private _tokenIdCounter;
 
-    // Define the MAX_SUPPLY and existingURIs variables as public variables
-    uint256 public MAX_SUPPLY = 9999; // Set max supply
-    mapping(string => uint8) public existingURIs; // Mapping to ensure unique URI
+    // Declaramos una variable pública para llevar un registro del límite de tokens que pueden ser emitidos
+    uint256 public MAX_SUPPLY = 9999;
 
-    // Define the constructor function
-    constructor() ERC721("NFT Contract", "NFT") {
-        // Initialize the counter with 1 to leave 0 available for the "null item"
-        _tokenIdCounter.set(1);
-    }
+    // Declaramos un mapeo público para llevar un registro de las URIs existentes
+    mapping(string => uint8) public existingURIs;
 
-    /**
-    * @notice Check if a token with the given URI has already been minted
-    * @param uri The URI of the token to check
-    * @return A boolean indicating whether the token has been minted
-    */
+    // El constructor inicializa el nombre y símbolo del token
+    constructor() ERC721("MyToken", "MTK") {}
+
+    // Esta función determina si una URI específica ha sido utilizada para crear un token
     function isOwned(string memory uri) public view returns (bool) {
-        // Return true if the token has been minted, false otherwise
+        // Devuelve true si la URI está en el mapeo y su valor es 1, false en caso contrario
         return existingURIs[uri] == 1;
     }
 
-    /**
-    * @notice Return the base URI for the contract
-    * @dev This function is required by the ERC721URIStorage contract and must be implemented
-    * @return The base URI for the contract
-    */
+    // Esta función es utilizada para obtener la URI base para los tokens
     function _baseURI() internal pure override returns (string memory) {
-        // Return the base URI for the contract
+        // Devuelve "ipfs://" como URI base para los tokens
         return "ipfs://";
     }
 
-    /**
-    * @notice Return the total supply of tokens in the contract
-    * @return The total supply of tokens
-    */
+    // Esta función permite consultar la cantidad de tokens emitidos
     function getSupply() public view returns (uint256) {
-        // Return the value of the supply variable
+        // Devuelve el valor de la variable 'supply' que lleva un registro de la cantidad de tokens emitidos
         return supply;
     }
 
-    /**
-    The payToMint function is a public function that can be called from outside the smart contract 
-    and can receive Ether as a payment. It takes in two input parameters: an address named recipient 
-    and a string named metadataURI.
+    // Esta función permite pausar el contrato
+    function pause() public onlyOwner {
+        // Llamamos a la función _pause() de la librería Pausable para pausar el contrato
+        _pause();
+    }
 
-    The function begins by checking if the value of existingURIs[metadataURI] is equal to 1. If it is, 
-    the function will revert and stop executing. This is to ensure that the token identified by metadataURI
-    has not already been minted.
+    // Esta función permite reanudar el contrato
+    function unpause() public onlyOwner {
+        // Llamamos a la función _unpause() de la librería Pausable para reanudar el contrato
+        _unpause();
+    }
 
-    Next, the function checks if the value of msg.value is greater than or equal to 0.0555 Ether. 
-    If it is not, the function will also revert and stop executing. This is to ensure that a payment 
-    of at least 0.0555 Ether is made to mint a new token.
-
-    If both of these checks are satisfied, the function will execute the following code:
-
-    1. It will retrieve the current value of _tokenIdCounter and store it in a variable named newToken.
-    2. It will increment the value of _tokenIdCounter.
-    3. It will set the value of existingURIs[metadataURI] to 1. This marks the token identified by 
-       metadataURI as having been minted.
-    4. It will call the internal function _mint and pass in the recipient and newToken as arguments. 
-       This function mints a new token and assigns it to the recipient address.
-    5. It will call the internal function _setTokenURI and pass in the newToken and metadataURI as arguments. 
-       This function associates the metadataURI with the newly minted token
-    */
-
+    // Esta función permite a los usuarios pagar para crear un nuevo token con una URI específica
     function payToMint(
+        // Recibe como argumento la dirección del receptor del nuevo token y la URI del nuevo token
         address recipient,
         string memory metadataURI
     ) public payable returns (uint256) {
+        // Verificamos que la URI del nuevo token no haya sido utilizada previamente para crear un token
         require(existingURIs[metadataURI] != 1, "This token has already been minted");
+        // Verificamos que el usuario haya enviado un pago de al menos 0.0555 ether para crear el nuevo token
         require (msg.value >= 0.0555 ether, "This contract requires a payment to mint");
 
+        // Obtenemos el siguiente ID de token disponible utilizando el contador _tokenIdCounter
         uint256 newToken = _tokenIdCounter.current();
+        // Incrementamos el contador para el próximo token
         _tokenIdCounter.increment();
+        // Actualizamos el mapeo de URIs existentes para indicar que la URI del nuevo token ya ha sido utilizada
         existingURIs[metadataURI] = 1;
 
+        // Llamamos a la función _mint() de la librería ERC721 para crear y transferir el nuevo token
         _mint(recipient, newToken);
+
+        // Llamamos a la función _setTokenURI() de la librería ERC721URIStorage para establecer la URI del nuevo token
         _setTokenURI(newToken, metadataURI);
 
+        
+// Devolvemos el ID del nuevo token
         return newToken;
     }
 
-    /**
-    The safeMint function is a public function that can only be called by the contract owner. 
-    It takes in two input parameters: an address named to and a string named uri.
-
-    The function begins by checking if the value of supply is less than MAX_SUPPLY. If it is not, 
-    the function will revert and stop executing, with the error message "Minted Out." This is to 
-    ensure that the maximum number of tokens allowed by the contract has not been reached.
-
-    If the check is satisfied, the function will execute the following code:
-
-    1. It will retrieve the current value of _tokenIdCounter and store it in a variable named tokenId.
-    2. It will increment the value of _tokenIdCounter.
-    3. It will call the internal function _mint and pass in the to and tokenId as arguments. This 
-    4. function mints a new token and assigns it to the to address.
-    5. It will call the internal function _setTokenURI and pass in the tokenId and uri as arguments. 
-    6. This function associates the uri metadata URI with the newly minted token identified by tokenId.
-    7. It will increment the value of supply by 1.
-    */
-
+    // Esta función permite al propietario del contrato crear y transferir un nuevo token con una URI específica
+    // Recibe como argumento la dirección del receptor del nuevo token y la URI del nuevo token
     function safeMint(address to, string memory uri) public onlyOwner {
+        // Verificamos que no se haya alcanzado el límite de tokens que pueden ser emitidos
         require(supply < MAX_SUPPLY, "Minted Out");
+        // Obtenemos el siguiente ID de token disponible utilizando el contador _tokenIdCounter
         uint256 tokenId = _tokenIdCounter.current();
+        // Incrementamos el contador para el próximo token
         _tokenIdCounter.increment();
-        _mint(to, tokenId);
+        // Llamamos a la función _safeMint() de la librería ERC721 para crear y transferir el nuevo token
+        _safeMint(to, tokenId);
+        // Llamamos a la función _setTokenURI() de la librería ERC721URIStorage para establecer la URI del nuevo token
         _setTokenURI(tokenId, uri);
+        // Incrementamos la cantidad de tokens emitidos
         supply++;
     }
 
-    /**
-    The _burn function is an internal function that can only be called from within the smart contract. 
-    It takes in one input parameter: an uint256 named tokenId.
-    The function simply calls the _burn function of the superclass (i.e., the ERC721 contract) and passes
-    in the tokenId as an argument. This function burns (i.e., destroys) the token identified by tokenId, 
-    removing it from circulation.
-    The override keyword indicates that this function is an override of a function with the same name 
-    defined in the ERC721 and ERC721URIStorage contracts that the contract is subclassing. This means 
-    that the implementation of the _burn function in this contract will replace the implementation in the 
-    superclass.
-    */
+    // Esta función es una sobrecarga (override) de la función _beforeTokenTransfer() de la librería ERC721
+    function _beforeTokenTransfer(
+        // Recibe como argumentos las direcciones de origen y destino, el ID del token y el tamaño del lote
+        address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        whenNotPaused
+        override
+    {
+        // Llamamos a la función _beforeTokenTransfer() de la librería ERC721 para ejecutar la funcionalidad original
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    // Esta función es una sobrecarga (override) de la función _burn() de la librería ERC721
+    // Esta función se ejecuta al quemar (borrar) un token
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        // Llamamos a la función _burn() de la librería ERC721 para ejecutar la funcionalidad original
         super._burn(tokenId);
     }
 
-    /**
-    The tokenURI function is a public function that can be called from outside the smart contract. 
-    It takes in one input parameter: an uint256 named tokenId.
-
-    The function calls the tokenURI function of the superclass (i.e., the ERC721URIStorage contract)
-    and passes in the tokenId as an argument. This function retrieves the metadata URI associated 
-    with the token identified by tokenId.
-
-    The view keyword indicates that this function is a read-only function and does not modify the 
-    state of the contract. The override keyword indicates that this function is an override of a 
-    function with the same name defined in the ERC721 and ERC721URIStorage contracts that the contract
-    is subclassing. This means that the implementation of the tokenURI function in this contract will 
-    replace the implementation in the superclass.
-    */
+    // Esta función es una sobrecarga (override) de la función tokenURI() de la librería ERC721URIStorage
+    // Esta función permite consultar la URI de un token específico
     function tokenURI(uint256 tokenId)
         public
         view
         override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
+        // Llamamos a la función tokenURI() de la librería ERC721URIStorage para ejecutar la funcionalidad original
         return super.tokenURI(tokenId);
     }
 }
+
